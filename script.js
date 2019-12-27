472aefbada3e710a776b46c17bfa41d3b1a37fa8
❖ KTT24 ❖ <kutter.thornton@gmail.com>
2019-12-27 21:44:17 +0000

    idk

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-const icons = Array.from(document.querySelectorAll('.icon'))
-icons.forEach((icon) => {
-  icon.addEventListener('click', () => {
-    icons.forEach((i) => {
-      i.classList.remove('icon--expanded');
-    });
-    icon.classList.add('icon--expanded')
-  })
-})
+let links = document.querySelectorAll('li.link');
+let buble = document.querySelector('.buble');
+
+links.forEach(el=>el.addEventListener('click',(e)=>{
+  document.querySelector('li.active').classList.remove('active')
+  el.classList.add('active');
+  buble.style.left = `${el.offsetLeft + 8}px`
+}))
